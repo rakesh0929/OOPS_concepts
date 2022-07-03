8aef9a93d6b668340030774ef11cade8f293a277
Rakesh <69312394+rakesh0929@users.noreply.github.com>
2022-07-03 17:01:32 +0530

    Update day_1.1.py

--- day_1.1.py
+++ day_1.1.py
@@ -14,28 +14,8 @@ r = person()
 print(m.emailid())
 
 
-#
-# # Task 1 - in a past whatever questions i have given to your with respect to list ,
-# # tuple , dic , set , string try to create a seprate class for each and everyting and
-# #     restructure your code for all the segment and submit .
-# #
-# # instruction number 1 -
-# #     always use exception handling
-# # instrruction number 2 :
-# #     use loggging as well
-# #
-# # Reformat your code and submit it to me before tomorrwo's' class 3 PM IST to my mail id
-# # sudhanshu@ineuron.ai
-# # sunny.savita@ineuron.ai
-# #
-# # i am only looking for your seperate python file in your github link
-#
-#
-#
-#
-#
-#
-#
+
+
 
 
 
